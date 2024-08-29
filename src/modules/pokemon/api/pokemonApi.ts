4173b029d8c6b37747a0fdc789f10d227a90379a
Alvarows20 <alvarojpt04.ws@gmail.com>
2024-08-28 20:38:16 -0500

    Configura la API de Pokémon para interacciones con la base de datos

--- src/modules/pokemon/api/pokemonApi.ts
+++ src/modules/pokemon/api/pokemonApi.ts
@@ -1,9 +1,11 @@
+
+//* Aquí tienes configurada la api para interactuar con la base de datos de Pokémon
 import axios from 'axios';
 
 
 
 const pokemonApi = axios.create({
-
+// * La baseUrl está configurada para apuntar la API de Pokémon, lo que facilita hacer peticiones GET para obtener información de los Pokémon
     baseURL: 'https://pokeapi.co/api/v2/pokemon',
 
 }
