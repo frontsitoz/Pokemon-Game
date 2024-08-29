4ba7da3c5473808cbe695634984d0dce40f656f0
Alvarows20 <alvarojpt04.ws@gmail.com>
2024-08-28 20:37:17 -0500

    Añade componente PokemonPicture para mostrar imágenes de Pokémon con estilos de visualización

--- src/modules/pokemon/components/PokemonPicture.vue
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -1,14 +1,31 @@
+//* Es es el componente que muestra la imagen de un Pokémon en blanco y negro. La imagen proviene de
+la URL de la API, y hemos aplicado estilos para evitar que la imagen sea arrastrable o
+seleccionable, lo que es buena práctica para mejorar la experiencia del usuario.
+
 <template>
   <section>
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
-      class="brightness-0 h-[200px]"
-      alt=""
-    />
+    <img v-if="!showPokemon" :src="pokemonImage" class="brightness-0 h-[200px]" alt="" />
+    <img v-else :src="pokemonImage" class="fade-in h-[200px]" alt="pokemonImage" />
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  pokemonId: number;
+  showPokemon: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showPokemon: false,
+});
+
+const pokemonImage = computed(
+  () =>
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.pokemonId}.svg`,
+);
+</script>
 
 //*Estos estilos son para que mi imagen no se pueda arrastrar, eso es de mala práctica
 <style scoped>
